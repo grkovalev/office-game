60a7962db3996b4f988d38d8e3560bc6620c9298
anastasions <>
2026-01-24 21:40:44 +0100

    correct images for winllost message

--- winlostmsg.gd
+++ winlostmsg.gd
@@ -3,12 +3,14 @@ extends Node2D
 @onready var winlostimg: Sprite2D = $winlostimg
 @onready var wintext: Label = $wintext
 @onready var losttext: Label = $losttext
+## Must be the full atlas image (e.g. winlostatlas.png), not an AtlasTexture.
 @export var win_atlas: Texture2D
 @export var win_atlas_columns: int = 4
 @export var win_cell_size: Vector2i = Vector2i(256, 512)
 @export var win_animation_speed: float = 0.3
+## Format for losttext; %s is replaced by the number of unflagged bombs. E.g. "%s" or "These new %s burning tasks are all yours!"
+@export var losttext_format: String = "%s"
 
-var _win_atlas_tex: AtlasTexture
 var _win_anim_timer: Timer
 var _win_frame: int = 0
 
@@ -22,39 +24,44 @@ func _ready() -> void:
 func _get_atlas_region(index: int) -> Rect2:
 	var col: int = index % win_atlas_columns
 	var row: int = index / win_atlas_columns
-	return Rect2(
-		Vector2(col * win_cell_size.x, row * win_cell_size.y),
-		Vector2(win_cell_size.x, win_cell_size.y)
-	)
+	return Rect2(col * win_cell_size.x, row * win_cell_size.y, win_cell_size.x, win_cell_size.y)
+
+func _get_atlas() -> Texture2D:
+	if win_atlas != null:
+		return win_atlas
+	return winlostimg.texture
+
+func _apply_region(index: int) -> void:
+	winlostimg.region_rect = _get_atlas_region(index)
+	winlostimg.queue_redraw()
 
 func _on_win_anim_tick() -> void:
 	_win_frame = 1 - _win_frame
-	if _win_atlas_tex:
-		_win_atlas_tex.region = _get_atlas_region(_win_frame)
+	_apply_region(_win_frame)
 
 func show_win() -> void:
 	_win_anim_timer.stop()
-	if win_atlas:
-		if _win_atlas_tex == null:
-			_win_atlas_tex = AtlasTexture.new()
-			_win_atlas_tex.atlas = win_atlas
+	var atlas := _get_atlas()
+	if atlas != null:
+		winlostimg.texture = atlas
+		winlostimg.region_enabled = true
 		_win_frame = 0
-		_win_atlas_tex.region = _get_atlas_region(0)
-		winlostimg.texture = _win_atlas_tex
+		_apply_region(0)
 		_win_anim_timer.wait_time = win_animation_speed
 		_win_anim_timer.start()
 	wintext.visible = true
 	losttext.visible = false
 	show()
 
-func show_lose(flagged_bomb_count: int = 0) -> void:
+func show_lose(flagged_bomb_count: int = 0, unflagged_bomb_count: int = 0) -> void:
 	_win_anim_timer.stop()
-	if win_atlas:
+	var atlas := _get_atlas()
+	if atlas != null:
 		var idx: int = clampi(11 - flagged_bomb_count, 0, 11)
-		var tex := AtlasTexture.new()
-		tex.atlas = win_atlas
-		tex.region = _get_atlas_region(idx)
-		winlostimg.texture = tex
+		winlostimg.texture = atlas
+		winlostimg.region_enabled = true
+		_apply_region(idx)
+	losttext.text = losttext_format % unflagged_bomb_count
 	wintext.visible = false
 	losttext.visible = true
 	show()
