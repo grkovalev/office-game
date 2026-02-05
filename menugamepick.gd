540ed5fe3a1cb45c1aff68904d63d8f776f53732
anastasions <>
2026-02-05 17:38:28 +0100

    working menu with exit and 2 games to pick

--- menugamepick.gd
+++ menugamepick.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
+const MINESWEEPER_SCENE := preload("res://minesweeper.tscn")
+const CALENDARTETRIS_SCENE := preload("res://calendartetris.tscn")
+
 @onready var exitbtn: TextureButton = $exitbtn
+@onready var flagbtn_minesweeper: TextureButton = get_node("01_task/minesweeper_btn/01flagbtn")
+@onready var flagbtn_calendartetris: TextureButton = get_node("02_task/tetris_btn/02flagbtn")
 
 func _ready() -> void:
 	exitbtn.pressed.connect(_on_exitbtn_pressed)
+	flagbtn_minesweeper.pressed.connect(_on_01flagbtn_pressed)
+	flagbtn_calendartetris.pressed.connect(_on_02flagbtn_pressed)
 
 func _on_exitbtn_pressed() -> void:
 	queue_free()
+
+func _on_01flagbtn_pressed() -> void:
+	queue_free()
+	var minesweeper := MINESWEEPER_SCENE.instantiate()
+	get_tree().root.add_child(minesweeper)
+
+func _on_02flagbtn_pressed() -> void:
+	queue_free()
+	var calendartetris := CALENDARTETRIS_SCENE.instantiate()
+	get_tree().root.add_child(calendartetris)
